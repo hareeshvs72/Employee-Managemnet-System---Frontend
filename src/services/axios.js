a9e7c4dd30de8e5e4bbeb021e4ca5853b33336c7
hareeshvs72 <hareeshvs72@gmail.com>
2026-06-24 20:43:00 +0530

    update the backend url

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
+    
     baseURL:(import.meta.env.VITE_BASE_URL  || "http://localhost:4000") + "/api"
 })
+    console.log(import.meta.env.VITE_BASE_URL);
 
 // attach token to all  api 
 
